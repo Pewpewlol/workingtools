1d540fe378683081183889d268a9b969d4572d16
Sandip Nijjar <sandip.nijjar17@gmail.com>
2025-10-31 19:19:31 +0100

    gitignore Update & urban spec mit Cookies akzeptieren

--- crawler/tests/urban.spec.ts
+++ crawler/tests/urban.spec.ts
@@ -9,27 +9,31 @@ test('Urban Rechnungen', async ({ page }) => {
 
   await page.goto('https://urbansportsclub.com/de/');
   await page.waitForTimeout(3000);
+  const cookieButton = page.getByRole('button', { name: 'Alle Cookies akzeptieren' });
+  if (await cookieButton.isVisible()) {
+        await cookieButton.click();
+  }
+  await page.waitForTimeout(1000);
+
   await page.getByRole('link', { name: 'Anmelden' }).click();
   await page.waitForTimeout(3000);
   await page.getByPlaceholder('E-Mail *').fill('sandip.nijjar17@gmail.com');
   await page.getByRole('textbox', { name: 'Passwort' }).fill('Fischfutter44.');
-  await page.getByRole('button', { name: 'Anmelden' }).click();
+  
+  const anmeldeButton = page.getByRole('button', { name: 'Anmelden' });
+  if (await anmeldeButton.isVisible() && await anmeldeButton.isEnabled()) {
+    await anmeldeButton.click();
+  }
   await page.waitForTimeout(5000);
   await page.goto('https://urbansportsclub.com/de/profile/payment-history');
-  await page.getByRole('button', { name: 'Akzeptieren' }).click();
-  await page.getByRole('button', { name: 'Close' }).click();
-  await page.getByRole('listitem').filter({ hasText: 'Sandip' }).getByRole('link').nth(1).click();
-  await page.getByRole('link', { name: 'Dein Konto' }).click();
-  await page.getByRole('link', { name: 'Rechnungsübersicht' }).click();
   const page1Promise = page.waitForEvent('popup');
   const downloadPromise = page.waitForEvent('download');
   await page.getByRole('link', { name: 'Rechnung herunterladen' }).first().click();
   const page1 = await page1Promise;
   await page1.pause();
   const download = await downloadPromise;
-  download.saveAs('./urbanrechnung/Rechnung.pdf');
+  download.saveAs('./urbanrechnung/Rechnung2.pdf');
   await wait(3000);
-  FileChecker.doesFileExist('./urbanrechnung/Rechnung.pdf');
+  FileChecker.doesFileExist('./urbanrechnung/Rechnung2.pdf');
 
-  
 });
