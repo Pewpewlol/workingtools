b3142a27f2a8e78805ed85522053a12ebb4cec86
Sandip Nijjar <sandip.nijjar17@gmail.com>
2025-11-02 21:13:51 +0100

    ops: Email Password richtig gesetzt

--- crawler/src/interfaces/out/email/emailService.ts
+++ crawler/src/interfaces/out/email/emailService.ts
@@ -10,13 +10,13 @@ export class EmailService {
   constructor(emailUser?: string, emailPassword?: string) {
     // Email-Konfiguration setzen
     this.emailUser = emailUser || process.env.EMAIL_USER || '';
-    this.emailPassword = emailPassword || process.env.EMAIL_PASS || process.env.EMAIL_PASSWORD || '';
+    this.emailPassword = emailPassword || process.env.EMAIL_PASSWORD || '';
     
     if (!this.emailUser || this.emailUser === '') {
       throw new Error("❌ EMAIL_USER ist nicht gesetzt");
     }
     if (!this.emailPassword || this.emailPassword === '') {
-      throw new Error("❌ EMAIL_PASS/EMAIL_PASSWORD ist nicht gesetzt");
+      throw new Error("❌ EMAIL_PASSWORD ist nicht gesetzt");
     }
 
     console.log(`📧 Email-Service initialisiert.`);
